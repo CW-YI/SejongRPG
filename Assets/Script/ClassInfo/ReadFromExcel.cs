30d39ade8a5e904417f7b0ac24c031ce788c594c
CW-YI <cwyi6104@gmail.com>
2024-12-26 19:34:27 +0900

    Read Text

--- Assets/Script/ClassInfo/ReadFromExcel.cs
+++ Assets/Script/ClassInfo/ReadFromExcel.cs
@@ -1,6 +1,8 @@
 using System.IO;
 using UnityEngine;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class ReadFromExcel : MonoBehaviour
 {
@@ -21,16 +23,28 @@ public class ReadFromExcel : MonoBehaviour
     {
         // 줄 단위로 나누기
         string[] lines = csvData.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+        var dataLines = lines.Skip(1); // LINQ의 Skip 사용
 
-        foreach (string line in lines)
+        foreach (string line in dataLines)
         {
             if (string.IsNullOrWhiteSpace(line)) continue; // 빈 줄 건너뛰기
 
             // 콤마(,)로 셀 구분
             string[] cells = line.Split(',');
-            Debug.Log(string.Join(" | ", cells)); // 셀 내용을 출력
+            //Debug.Log(string.Join(" | ", cells)); // 셀 내용을 출력
 
+            string major = cells[0];
+            string id = cells[1];
+            string subjectName = cells[2];
+            string category = cells[3];
+            Debug.Log(cells[4]);
+            int grade = int.Parse(cells[4]);
+            int credit = (int)(float.Parse(cells[5]));
+            bool isCompleted = false;
+            List<string> otherPoints = new List<string>();
 
+            Class newClass = new Class(major, id, subjectName, category, grade, credit, isCompleted, otherPoints);
+            classInfo.totalClasses.Add(newClass);
         }
     }
 }
