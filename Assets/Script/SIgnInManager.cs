b777eab3dbdbafbe7936f6b9169f3f74a836a67a
zzani0207 <87990208+zzani0207@users.noreply.github.com>
2024-12-26 13:02:40 +0900

    Update SIgnInManager.cs

--- Assets/Script/SIgnInManager.cs
+++ Assets/Script/SIgnInManager.cs
@@ -1,17 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 using TMPro;
 
-public class SignInManager : MonoBehaviour
+public class LoginManager : MonoBehaviour
 {
     public TMP_InputField idInputField;
     public TMP_InputField passwordInputField;
     public Button loginButton;
 
-    private string backendUrl = "http://your-backend-url.com/api/user"; // 백엔드 서버의 엔드포인트 URL
+    private string loginUrl = "http://your-backend-url.com/api/login"; // 백엔드 서버의 로그인 엔드포인트 URL
 
     // Start is called before the first frame update
     void Start()
@@ -19,25 +18,15 @@ public class SignInManager : MonoBehaviour
         loginButton.onClick.AddListener(OnLoginButtonClick);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnLoginButtonClick()
     {
-        string username = idInputField.text;
+        int id = int.Parse(idInputField.text);
         string password = passwordInputField.text;
 
         User user = new User
         {
-            username = username,
-            password = password,
-            name = "Temporary Name", // 임시 값
-            year = 1, // 임시 값
-            creditsEarned = 0, // 임시 값
-            creditsRequired = 120 // 임시 값
+            ID = id,
+            password = password
         };
 
         SendUserData(user);
@@ -52,7 +41,7 @@ public class SignInManager : MonoBehaviour
     {
         string jsonData = JsonUtility.ToJson(user);
 
-        UnityWebRequest request = new UnityWebRequest(backendUrl, "POST");
+        UnityWebRequest request = new UnityWebRequest(loginUrl, "POST");
         byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
         request.uploadHandler = new UploadHandlerRaw(bodyRaw);
         request.downloadHandler = new DownloadHandlerBuffer();
@@ -77,11 +66,13 @@ public class SignInManager : MonoBehaviour
         {
             User user = JsonUtility.FromJson<User>(jsonResponse);
             Debug.Log("User data received:");
-            Debug.Log("Username: " + user.username);
             Debug.Log("Name: " + user.name);
-            Debug.Log("Year: " + user.year);
-            Debug.Log("Credits Earned: " + user.creditsEarned);
-            Debug.Log("Credits Required: " + user.creditsRequired);
+            Debug.Log("ID: " + user.ID);
+            Debug.Log("User Grade: " + user.userGrade);
+            Debug.Log("Credit Sum: " + user.creditSum);
+            Debug.Log("Military Service Needed: " + user.militaryServiceNeeded);
+            Debug.Log("Military Served: " + user.militaryServed);
+            Debug.Log("Major Transfer: " + user.majorTransfer);
             // 추가적인 처리 로직을 여기에 작성
         }
         catch (System.Exception ex)
